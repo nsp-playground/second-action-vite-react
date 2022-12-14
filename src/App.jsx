c05711ca536037a6f40a0fcd177e52358438679b
Nazmuz Pranto <npranto4@gmail.com>
2022-12-14 13:29:07 -0500

    updates app.jsx and adds jest test

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,19 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Second Action: Vite + React</h1>
+      <h3>{count}</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrement
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,5 @@
+describe('Example Test', () => { 
+  it('should return true', () => {
+    expect(1 === 1).toBe(true);
+  })
+})
